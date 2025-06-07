627a022ca9076654a91f0e0e1117beb1db6b98c2
inu255 <artmvhdv@mail.ru>
2025-06-07 18:52:15 +0300

    update vite config 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
 import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 export default defineConfig({
   base: "/banka/",
@@ -36,7 +37,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      src: "/src",
+      src: path.resolve(__dirname, "src"),
     },
   },
 });
